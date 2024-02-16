ec9db67303540c6a79a0d1c693a327e6546f7e01
jacecil <bravenewcliche@gmail.com>
2024-02-16 04:38:05 -0700

    Testing

--- src/main/java/net/jaar/edibleberries/EdibleBerries.java
+++ src/main/java/net/jaar/edibleberries/EdibleBerries.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 public class EdibleBerries implements ModInitializer {
     public static final String MOD_ID = "edible-berries";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	//test
 
 	@Override
 	public void onInitialize() {
