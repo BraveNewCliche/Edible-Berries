68527543ca6180ca8e12f7379d8266940c77a209
Jacob Cecil <jacecil@Jacobs-MacBook-Air.local>
2024-02-16 04:35:11 -0700

    Initial commit

---
+++ src/main/java/net/jaar/edibleberries/EdibleBerriesClient.java
@@ -0,0 +1,10 @@
+package net.jaar.edibleberries;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EdibleBerriesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
