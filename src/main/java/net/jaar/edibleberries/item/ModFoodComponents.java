b276ff7c090d8973b3e0638ce54dce9a13a04c2b
jacecil <bravenewcliche@gmail.com>
2024-02-18 22:33:11 -0700

    Changed to allow all cobblemon berries to be cooked utilizing the "berry" tag

--- src/main/java/net/jaar/edibleberries/item/ModFoodComponents.java
+++ src/main/java/net/jaar/edibleberries/item/ModFoodComponents.java
@@ -3,5 +3,5 @@ package net.jaar.edibleberries.item;
 import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponents {
-    public static final FoodComponent BakedBerry = new FoodComponent.Builder().hunger(4).saturationModifier(0.2f).build();
+    public static final FoodComponent BakedBerry = new FoodComponent.Builder().hunger(2).saturationModifier(0.2f).build();
 }
